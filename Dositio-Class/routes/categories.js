3d0ce69979b586cbd93c995e7f5312853604576e
PedroBelo131 <103450336+PedroBelo131@users.noreply.github.com>
2024-03-25 08:37:00 -0300

    Add files via upload

---
+++ Dositio-Class/routes/categories.js
@@ -0,0 +1,67 @@
+/** @type{import('fastify').FastifyPluginAsync<>} */
+import createError from '@fastify/error';
+export default async function categories(app, options) {
+    const InvalidcategoriesError = createError('InvalidcategoriesError', 'categoria Inválido.', 400);
+
+    const categories = app.mongo.db.collection('categories');
+
+
+    app.get('/categories', 
+        async (request, reply) => {
+            request.log.info(categories);
+        return await categories.find().toArray();
+    });
+
+    app.post('/categories', {
+        schema: {
+            body: {
+                type: 'object',
+                properties: {
+                    id: { type: 'integer' },
+                    name: { type: 'string' }
+                },
+                required: ['name']
+            }
+        },
+        config: {
+            requireAuthentication: true
+        }
+    }, async (request, reply) => {
+        let product = request.body;
+        
+        await categories.insertOne(product);
+
+        return reply.code(201).send();
+    });
+
+    app.put('/categories/:id', {
+        config: {
+            requireAuthentication: true
+        }
+    }, async (request, reply) => {
+        let id =  request.params.id;
+        let product = request.body;
+        
+        await categories.updateOne({_id: new app.mongo.ObjectId(id)}, {
+            $set: {
+                name: product.name,
+                qtd: product.qtd
+            }
+        });
+        
+        return reply.code(204).send();;
+    });
+
+    app.delete('/categories/:id', {
+        config: {
+            requireAuthentication: true
+        }
+    }, async (request, reply) => {
+        let id =  request.params.id;
+        
+        await categories.deleteOne({_id: new app.mongo.ObjectId(id)});
+        
+        return reply.code(204).send();;
+    });
+
+}
